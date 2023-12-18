6827d6925dae006cdc3ce6a24cf0c2d9e91062a3
Jack Krause <jmkrause@iastate.edu>
2023-12-18 14:58:21 -0600

    rough draft of code

--- Problems.py
+++ Problems.py
@@ -3,7 +3,6 @@ from typing import List
 
 class Solution:
 
-
     def reverseWords(self, s: str) -> str:
         """151. Reverse Words in a String (Medium)
 
@@ -23,7 +22,6 @@ class Solution:
 
         return ret.strip()
 
-
     def longestCommonPrefix(self, strs: List[str]) -> str:
         """14. Longest Common Prefix (Easy)
 
@@ -34,29 +32,31 @@ class Solution:
         A string of the longest common prefix, or an empty string otherwise.
         """
 
-        #get all substrings
+        # get all substrings
         n = len(strs)
         prefixes = [""] * n
         counter = 0
 
         # while there are duplicate prefixes
-        while len(set(prefixes)) < n:
-            #iterate through strings and add prefix character
+        while True:
+            if len(set(prefixes)) > 1:
+                break
+            # iterate through strings and add prefix character
             for i in range(n):
                 element = strs[i]
-                if (counter < len(element)):
+                if (counter < len(element)) and element[counter] != " ":
                     prefixes[i] += element[counter]
-                if i == n-1:
+                if i == n - 1:
                     counter += 1
 
+
         print(prefixes)
-
-
-
-
+        ret = {el for el in prefixes if prefixes.count(el) > 1}
+        print(ret)
 
 
 solution_inst = Solution()
 # temp = ["hello", "there", "hi", "name"]
-temp = ["hello", "hello there", "hi", "helloooo"]
+# temp = ["hello", "hello there", "hi", "helloooo"]
+temp = ["flower", "flow", "flight"]
 solution_inst.longestCommonPrefix(temp)
