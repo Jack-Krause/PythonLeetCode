fd128c8249f3c7c329410487aade010271b0194f
Jack Krause <jmkrause@iastate.edu>
2023-12-18 16:35:01 -0600

    in progress

--- Problems.py
+++ Problems.py
@@ -37,6 +37,12 @@ class Solution:
         if n == 0:
             return ""
 
+        elif n == 1:
+            return strs[0]
+
+        elif all(strs[t] == strs[0] for t in range(len(strs))):
+            return strs[0]
+
         substrings = [""] * n
         counter = 0
 
@@ -50,11 +56,18 @@ class Solution:
             if all(substrings[k] == "" for k in range(n)):
                 break
 
-        return list(set(substrings[:-1]))[0][:-1]
-        # print(list(set(substrings[:-1]))[0][:-1])
+        if (len(set(substrings))) == 0:
+            print("a")
+            return list(set(substrings))[0]
+        else:
+            print("b")
+            return min(list(set(substrings)), key=len)[:-1]
+
 
 solution_inst = Solution()
 # temp = ["hello", "there", "hi", "name"]
 # temp = ["hello", "hello there", "hi", "helloooo"]
 temp = ["flower", "flow", "flight"]
-solution_inst.longestCommonPrefix(temp)
+# temp = ["aaa","aa","aaa"]
+print(solution_inst.longestCommonPrefix(temp))
+# solution_inst.longestCommonPrefix(temp)
